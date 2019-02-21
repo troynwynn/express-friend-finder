97391b05aae376b0137387b0c0366cd17a5efdb4
Troy Wynn <troynwynn@gmail.com>
2019-02-21 00:39:24 -0500

    fixed finder function 12:39PM 2.21.19

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,22 +3,26 @@ var friends = require(`../data/friends`);
 
 module.exports = function(app) {
 
-    function friendFinder(newFriend, friends) {
+    function friendFinder(bol, friends) {
         var bestFriend = [];
+    
         
         for (let i=0; i<friends.length-1; i++) {
             var diff = 0
     
             for (let j=0; j<10; j++) {
-                newFriendScores = newFriend.scores[j];
+                newFriendScores = bol.scores[j];
                 currentFriendScores = friends[i].scores[j];
                 diff += Math.abs(newFriendScores-currentFriendScores);
-                if (i==0) {
-                    currentMin = diff;
-                }
+                
+                
             }  
-            
-            if (currentMin <= diff) {
+    
+            if (i==0) {
+                currentMin = diff;
+            }
+    
+            if (currentMin >= diff) {
                 currentMin = diff;
                 bestFriend = friends[i]
             }
@@ -28,6 +32,7 @@ module.exports = function(app) {
         return bestFriend;
     
     }
+    
 
     app.get("/api/friends", function(req, res) {
         res.json(friends);
@@ -45,7 +50,7 @@ module.exports = function(app) {
 
         friends.push(newFriend);
 
-        friendFinder(newFriend, friends);
+        console.log(friendFinder(newFriend, friends));
 
         res.json(friendFinder(newFriend, friends));
 
